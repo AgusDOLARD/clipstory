706f409d2a4186780622e41c69e5b0bcb13af433
AgusDOLARD <agust_22_10@hotmail.com>
2024-08-11 20:04:20 -0300

    fix: not copying to clipboard

--- cmd/commands/pick.go
+++ cmd/commands/pick.go
@@ -20,8 +20,12 @@ func (command *Command) Pick() *cli.Command {
 				return cli.Exit(err.Error(), 1)
 			}
 
+			if len(clips) == 0 {
+				return cli.Exit("No clipboard history", 0)
+			}
+
 			var pick string
-			err = picker(clips, pick)
+			err = picker(clips, &pick)
 			if err != nil {
 				if errors.Is(err, huh.ErrUserAborted) {
 					return nil
@@ -30,18 +34,26 @@ func (command *Command) Pick() *cli.Command {
 				return cli.Exit(err.Error(), 1)
 			}
 
-			clipboard.Write(clipboard.FmtText, []byte(pick))
-			return nil
+			return copyToClipboard([]byte(pick))
 		},
 	}
 }
 
-func picker(clips []string, value string) error {
+func picker(clips []string, value *string) error {
 	return huh.NewSelect[string]().
 		Title("Pick a clip").
 		Options(huh.NewOptions(clips...)...).
-		Filtering(true).
-		Value(&value).
+		// Filtering(true).
+		Value(value).
 		WithTheme(huh.ThemeBase16()).
 		Run()
+}
+
+func copyToClipboard(b []byte) error {
+	err := clipboard.Init()
+	if err != nil {
+		return err
+	}
+	<-clipboard.Write(clipboard.FmtText, b)
+	return nil
 }
